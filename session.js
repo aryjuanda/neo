eea065b27ca072ef8b3cba9de8ef04bc808efd31
Doumaut <108480751+Doumaut@users.noreply.github.com>
2022-07-02 01:32:37 +0700

    Update and rename settings/options/config.js to session.js

--- session.js
+++ session.js
@@ -1,15 +1,15 @@
-// CREATED BY LEXXY
+// CREATED BY Balls
 const fs = require('fs')
 const chalk = require('chalk')
 
 // OTHERS
-global.owner = ['994408465157', '994408465157']
-global.premium = ['994408465157']
-global.number = '994408465157'
-global.botname = 'AryBotz-MD'
-global.packname = 'ByAry-MD'
-global.author = 'By Ary-Caa-!'
-global.emailowner = 'arybotz791@gmail.com'
+global.owner = ['6285855884435', '6285738856584']
+global.premium = ['6285855884435']
+global.number = '6281359101470'
+global.botname = 'Ballsstorebot-MD'
+global.packname = 'ByBalls-MD'
+global.author = 'By Balls-store-!'
+global.emailowner = 'marogol56@gmail.com'
 global.websiteowner = 'https://github.com/aryjuanda'
 global.region = 'Indonesia'
 global.sessionName = "session"
@@ -19,7 +19,7 @@ global.simbol = "➭"
 global.rkyt = []
 
 // BIARIN:v
-global.creator = '© Created By AryBotz Official'
+global.creator = '© Created By Balls Official'
 
 // MESSAGE
 global.mess = {
